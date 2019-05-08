2fdcc5b5db2b64e9a18b27ef9cc23dbb16569ce4
Agent47 <654136828@qq.com>
2019-05-08 17:03:54 +0800

    update

---
+++ leetcode/283.移动零.js
@@ -0,0 +1,36 @@
+/*
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+示例:
+
+输入: [0,1,0,3,12]
+输出: [1,3,12,0,0]
+说明:
+
+必须在原数组上操作，不能拷贝额外的数组。
+尽量减少操作次数。
+ * */
+/**
+ * 根据提议不能使用 数组的 filter splice slice 等生成数组的方法
+ * */
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let i = 0, j = 0;
+    for (; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            // j 为 除去 0 后目标能 i 下标不为 0 数据 放数据的位置(数据不为 0 会 + 1)
+            nums[j++] = nums[i];
+        }
+    }
+    while (j < nums.length) {
+        nums[j++] = 0;
+    }
+};
+
+const sample = [1, 0, 0, 1];
+moveZeroes(sample);
+const assert = require("assert");
+assert.deepStrictEqual(sample, [1, 1, 0, 0]);
---
+++ leetcode/4.寻找两个有序数组的中位数.js
@@ -0,0 +1,32 @@
+/*
+给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+
+请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+
+你可以假设 nums1 和 nums2 不会同时为空。
+
+示例 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+则中位数是 2.0
+示例 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+则中位数是 (2 + 3)/2 = 2.5
+* */
+/**
+ * 由于规定了时间复杂度 所以不能排序
+ * */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function (nums1, nums2) {
+
+
+};
