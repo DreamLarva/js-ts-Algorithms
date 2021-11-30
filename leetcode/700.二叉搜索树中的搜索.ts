cdb39360904b0627b3a5b8576e4e0f4e9c1ab60b
yangjiaqi <yangjiaqi@orientsec.com.cn>
2021-11-30 09:40:59 +0800

    2021/11/30 1

---
+++ leetcode/700.二叉搜索树中的搜索.ts
@@ -0,0 +1,56 @@
+/*
+700. 二叉搜索树中的搜索
+给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+
+例如，
+
+给定二叉搜索树:
+
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+和值: 2
+你应该返回如下子树:
+
+      2
+     / \
+    1   3
+在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+
+
+* */
+
+// Definition for a binary tree node.
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+  if (root == null) {
+    return null;
+  }
+
+  if (root?.val === val) {
+    return root;
+  }
+
+  if (root?.val > val) {
+    return searchBST(root.left, val);
+  }
+
+  return searchBST(root.right, val);
+}
+
+
+
+export {};
